eec90108d4b2ac3a1e5fabae35d56b65adba308d
bunbook <25857032+bunbook@users.noreply.github.com>
2018-05-03 15:15:28 +0900

    [Add] もろもろ

---
+++ Assets/UnityChangTest/Scripts/UnityChangController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityChangController : MonoBehaviour
+{
+
+    #region フィールド
+
+    private Transform _transform;
+    private Animator animator;
+
+    private float moveSpeed;
+    private Vector3 moveDirection;
+
+    #endregion
+
+
+    #region プロパティ
+
+    private bool IsRunning { get { return moveDirection.magnitude != 0f; } }
+
+    #endregion
+
+
+    #region Unity メソッド
+
+    /// <summary> 
+    /// 初期化処理
+    /// </summary>
+    void Awake()
+    {
+        _transform = transform;
+        animator = GetComponent<Animator>();
+
+        moveSpeed = 5.0f;
+        moveDirection = Vector3.zero;
+    }
+
+    /// <summary> 
+    /// 更新処理
+    /// </summary>
+    void Update()
+    {
+        InputDirection();
+        Move();
+        Turn();
+        Animate();
+    }
+
+    #endregion
+
+
+    #region メソッド
+
+    public void InputDirection()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+        moveDirection = new Vector3(x, 0f, z);
+    }
+
+    private void Move()
+    {
+        _transform.position += moveDirection.normalized * moveSpeed * Time.deltaTime;
+    }
+
+    private void Turn()
+    {
+        if (!IsRunning) return;
+        Quaternion targetRotation = Quaternion.LookRotation(moveDirection.normalized);
+        _transform.rotation = Quaternion.RotateTowards(_transform.rotation, targetRotation, 360f * Time.deltaTime);
+    }
+
+    private void Animate()
+    {
+        animator.SetBool("isRunning", IsRunning);
+    }
+
+    #endregion
+
+}
---
+++ Assets/UnityChangTest/Scripts/UnityChangControllerForTPV.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityChangControllerForTPV : MonoBehaviour
+{
+
+    #region フィールド
+
+    private Transform _transform;
+    private Rigidbody _rigidbody;
+    private Animator animator;
+
+    private float moveSpeed;
+    private float rotateSpeed;
+    private Vector3 moveDirection;
+    private Vector3 rotateDirection;
+
+    #endregion
+
+
+    #region プロパティ
+
+    private bool IsRunning { get { return moveDirection.z > 0f; } }
+    private bool IsTurning { get { return rotateDirection.magnitude != 0f; } }
+
+    #endregion
+
+
+    #region Unity メソッド
+
+    /// <summary> 
+    /// 初期化処理
+    /// </summary>
+    void Awake()
+    {
+        _transform = transform;
+        _rigidbody = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+
+        moveSpeed = 5.0f;
+        rotateSpeed = 3.0f;
+        moveDirection = Vector3.zero;
+        rotateDirection = Vector3.zero;
+    }
+
+    /// <summary> 
+    /// 更新処理
+    /// </summary>
+    void Update()
+    {
+        InputDirection();
+    }
+
+    void FixedUpdate()
+    {
+        Turn();
+        Move();
+        Animate();
+    }
+
+    #endregion
+
+
+    #region メソッド
+
+    public void InputDirection()
+    {
+        float y = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector3(0f, 0f, z);
+        rotateDirection = new Vector3(0f, y, 0f);
+    }
+
+    private void Move()
+    {
+        if (!IsRunning)
+        {
+            _rigidbody.velocity = Vector3.zero;
+            return;
+        }
+        _rigidbody.velocity = _transform.forward * moveSpeed;
+    }
+
+    private void Turn()
+    {
+        if (!IsTurning)
+        {
+            _rigidbody.angularVelocity = Vector3.zero;
+            return;
+        }
+        _rigidbody.angularVelocity = rotateDirection * rotateSpeed;
+    }
+
+    private void Animate()
+    {
+        animator.SetBool("isRunning", IsRunning);
+    }
+
+    #endregion
+
+}
